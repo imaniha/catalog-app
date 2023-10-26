36741fbab1d73c8481fd2e7b8f1f50e3464fcaad
Imaniha <cvs.usagers@gmail.com>
2023-10-27 00:23:00 +0200

    Activate testData Profiles for dev environment.

---
+++ src/main/java/org/akt/demo/BookDataLoader.java
@@ -0,0 +1,25 @@
+package org.akt.demo;
+
+import org.akt.domain.Book;
+import org.akt.domain.BookRepository;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("testData")
+public class BookDataLoader {
+    private final BookRepository bookRepository;
+    public BookDataLoader(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void loadBookTestData() {
+        var book1 = new Book("1231231234", "Northern Lights", "Lyra Silverstar", 9.90);
+        var book2 = new Book("1231231235", "Disc World", "Terry Pratchett", 9.90);
+        bookRepository.save(book1);
+        bookRepository.save(book2);
+    }
+}
