df1c9e6811c8725a4f8acae7dc4366f8d5a48510
Imaniha <cvs.usagers@gmail.com>
2023-11-03 23:43:58 +0100

    Add postgresql testcontainers dependency.

---
+++ src/test/java/org/akt/domaine/BookRepositoryJdbcTests.java
@@ -0,0 +1,53 @@
+package org.akt.domaine;
+
+import org.akt.configuration.DataConfig;
+import org.akt.domain.Book;
+import org.akt.domain.BookRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jdbc.core.JdbcAggregateTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import java.util.Optional;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJdbcTest
+@Import(DataConfig.class)
+@AutoConfigureTestDatabase(
+        replace = AutoConfigureTestDatabase.Replace.NONE
+)
+@ActiveProfiles("integration")
+public class BookRepositoryJdbcTests {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private JdbcAggregateTemplate jdbcAggregateTemplate;
+
+    @Test
+    void findBookByIsbnWhenExisting() {
+        var bookIsbn = "1231231234";
+        var book = Book.of(bookIsbn, "Title", "Author", 9.90);
+
+        jdbcAggregateTemplate.insert(book);
+        Optional<Book> actualBook = bookRepository.findByIsbn(bookIsbn);
+
+        assertThat(actualBook).isPresent();
+        assertThat(actualBook.get().isbn()).isEqualTo(book.isbn());
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
