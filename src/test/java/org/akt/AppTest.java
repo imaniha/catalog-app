f2d2bd6156d4e1bca10c1317c4cf1a6e967ce504
Imaniha <cvs.usagers@gmail.com>
2023-11-09 01:59:27 +0100

    Setup database migration

--- src/test/java/org/akt/AppTest.java
+++ src/test/java/org/akt/AppTest.java
@@ -5,12 +5,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
+@ActiveProfiles("integration")
 public class AppTest {
 
   @Autowired
